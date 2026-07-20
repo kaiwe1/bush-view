146baceb44b085ed53d804a3eac8b0c0a5affddb
Kaiwei Zhang <kaiwei.zhqwq@gmail.com>
2026-07-20 20:15:47 +0800

    chore(debug): add Electron main process debugging setup
      - enable source maps for the main process bundle
      - add VS Code configuration for attaching to Electron

--- vite.main.config.ts
+++ vite.main.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from 'vite';
 // https://vitejs.dev/config
 export default defineConfig({
   build: {
+    sourcemap: true,
     rollupOptions: {
       output: {
         entryFileNames: 'main.js',
